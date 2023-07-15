c8e378e1e0d21d3fbfeb9e5d3793dd345fb675ea
Ian Connell <ian10gt@gmail.com>
2023-07-15 17:03:59 +0100

    57: using await findBy to Find Elements that Populate Asynchronously

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,15 +1,15 @@
-import {render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import Options from "../Options";
 
-import Options from '../Options'
+test("displays image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
 
-test('displays image for each scoop from the server', () => {
-    render(<Options optionType="scoops" />)
+  // find images
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
 
-    // find images
-    const scoopImages = screen.getAllByRole('img', { name: /scoop$/i });
-    expect(scoopImages).willHaveLength(2);
-
-    // confirm alt text of imges
-    const altText = scoopImages.map((element) => element.alt);
-    expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
-})
+  // confirm alt text of images
+  // @ts-ignore
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from '@testing-library/user-event'
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
-test("initial onditionst", () => {
+test("Initial conditions", () => {
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
@@ -13,7 +13,7 @@ test("initial onditionst", () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test("checking checkbox enables button", async () => {
+test("Checkbox enables button on first click and disables on second click", async () => {
   const user = userEvent.setup();
 
   render(<SummaryForm />);
@@ -24,27 +24,31 @@ test("checking checkbox enables button", async () => {
 
   await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
+
   await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
 
-test('popover response to hover', async () => {
+test("popover responds to hover", async () => {
   const user = userEvent.setup();
   render(<SummaryForm />);
 
-  //popover starts out hidden
-  const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
   expect(nullPopover).not.toBeInTheDocument();
-  //popover appears on mouseover of checkbox label
+
+  // popover appears on mouseover of checkbox label
   const termsAndConditions = screen.getByText(/terms and conditions/i);
   await user.hover(termsAndConditions);
   const popover = screen.getByText(/no ice cream will actually be delivered/i);
   expect(popover).toBeInTheDocument();
 
-  //popover disappears when we mouse out
+  // popover disappears when we mouse out
   await user.unhover(termsAndConditions);
   expect(popover).not.toBeInTheDocument();
-})
+});
 
 // import { render, screen } from "@testing-library/react";
 // import SummaryForm from "../SummaryForm";
