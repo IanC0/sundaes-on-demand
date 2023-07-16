41c4c53f30c02f4a46aa35cfd868daa21faedf50
Ian Connell <ian10gt@gmail.com>
2023-07-16 15:18:14 +0100

    69: React Code: OrderDetails Context

---
+++ src/pages/entry/tests/TotalUpdates.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Options from "../Options";
+
+test("update scoop subtotal when scoops change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="scoops" />);
+
+  const toppings = await screen.findAllByRole("img");
+  expect(toppings).toHaveLength(2);
+
+  // make sure total starts out $0.00
+  const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
+
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+  expect(scoopsSubtotal).toHaveTextContent("2.00");
+
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, "2");
+  expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
